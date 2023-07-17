025759cb2903dc4499bf8ec4b568994c8d9dc051
Ali İbrahim Keçeci <aibrahimkcc@gmail.com>
2023-07-17 18:26:19 +0300

    csv dosyalarini array haline getirdim.

--- app/App.php
+++ app/App.php
@@ -1,5 +1,41 @@
 <?php
-
 declare(strict_types = 1);
+$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
+$dir=(scandir(FILES_PATH));
+$csvFiles = array_slice($dir, 2);
+//var_dump($dir);
+$array=[];
+foreach($csvFiles as $csvfile){
+    if (($handle = fopen(FILES_PATH.$csvfile, "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000)) !== FALSE) {
+            $num = count($data);
+            array_push($array,$data);
 
-// Your Code
+        }
+        
+        fclose($handle);
+    }
+}
+echo '<pre>';
+print_r($array);
+echo '<pre/>';
+
+//$a=fopen(FILES_PATH.$csvFiles[0],'r');
+////fopen(FILES_PATH.$csvFiles[1],'r');
+//
+//while(($line = fgets($a))!==false){
+//    echo $line. '<br/>';
+//}
+//echo '<br/>';
+//var_dump($a);
+//echo '<br/>';
+//while(($line = fgets($a))!==false){
+//    echo $line. '<br/>';
+//}
+
+file_put_contents('toplam.csv',$csvFiles,FILE_APPEND);
+
+//$asd=fopen(FILES_PATH . DIRECTORY_SEPARATOR. 'sample_1.csv','r');
+//$csv=fgetcsv($asd);
+//print_r($csv);
--- public/index.php
+++ public/index.php
@@ -1,11 +1,82 @@
 <?php
 
 declare(strict_types = 1);
-
+//dirname:Returns a parent directory's path
 $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
 define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
 define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
+$dir=scandir(FILES_PATH);
+var_dump($dir);
+
+
+
 /* YOUR CODE (Instructions in README.md) */
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Transactions</title>
+        <style>
+            table {
+                width: 100%;
+                border-collapse: collapse;
+                text-align: center;
+            }
+
+            table tr th, table tr td {
+                padding: 5px;
+                border: 1px #eee solid;
+            }
+
+            tfoot tr th, tfoot tr td {
+                font-size: 20px;
+            }
+
+            tfoot tr th {
+                text-align: right;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Check #</th>
+                    <th>Description</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- YOUR CODE -->
+                <?php
+                //<tr>
+                //    <td>Date</td>
+                //    <td>Check </td>
+                //    <td>Description</td>
+                //    <td>Amount</td>
+                //</tr>
+                ?>
+
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th colspan="3">Total Income:</th>
+                    <td><!-- YOUR CODE --></td>
+                </tr>
+                <tr>
+                    <th colspan="3">Total Expense:</th>
+                    <td><!-- YOUR CODE --></td>
+                </tr>
+                <tr>
+                    <th colspan="3">Net Total:</th>
+                    <td><!-- YOUR CODE --></td>
+                </tr>
+            </tfoot>
+        </table>
+    </body>
+</html>
